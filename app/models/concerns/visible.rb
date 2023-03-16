5637b64ffd537512be983f5aa43cf1e82dc85678
Trang Tung Nguyen <trangtungn@gmail.com>
2023-03-17 00:01:59 +0700

    Add status pending

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,6 +1,6 @@
 module Visible
   extend ActiveSupport::Concern
   included do
-    enum :status, active: 'active', inactive: 'inactive', archived: 'archived'
+    enum :status, active: 'active', inactive: 'inactive', archived: 'archived', pending: 'pending'
   end
 end
