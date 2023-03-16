2f12b301a2f3946ea94ea55955eaef112abc355c
Trang Tung Nguyen <trangtungn@gmail.com>
2023-03-17 00:01:49 +0700

    Add message model

---
+++ app/models/message.rb
@@ -0,0 +1,3 @@
+class Message < ApplicationRecord
+  include Visible
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_02_061320) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_162248) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.string "status", default: "active"
     t.integer "lock_version"
   end
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
-    t.text "body"
-    t.integer "article_id", null: false
+    t.text "content"
+    t.bigint "article_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
+    t.string "status", default: "pending"
     t.index ["article_id"], name: "index_comments_on_article_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.string "subject"
+    t.text "body"
+    t.string "status", default: "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "comments", "articles"
