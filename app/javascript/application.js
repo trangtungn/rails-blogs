379d9c30b4c68fd4086464920909d7e8ff855a02
Trang Tung Nguyen <trangtungn@gmail.com>
2023-03-28 23:14:22 +0700

    Import turbo rails controllers

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// disable the Turbo Drive framework
+// Turbo.session.drive = false
