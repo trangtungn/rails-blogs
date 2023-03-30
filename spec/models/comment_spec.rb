1c2560ba313a5344f9825d4fc37a45257cc663af
Trang Tung Nguyen <trangtungn@gmail.com>
2023-03-31 00:41:35 +0700

    Update comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe Comment, type: :model do
       with_values(active: 'active', inactive: 'inactive', archived: 'archived', pending: 'pending').
       backed_by_column_of_type(:string)
   }
+
+  it { expect(build(:comment)).to be_valid }
+
+  describe "#title" do
+    let(:comment) { create(:comment) }
+
+    before { comment }
+
+    it { expect(comment.title).to eq(comment.content[..128]) }
+  end
 end
