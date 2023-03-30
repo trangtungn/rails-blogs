437388e84d828a6e6a668a54f714dc68fd9384b4
Trang Tung Nguyen <trangtungn@gmail.com>
2023-03-31 00:41:44 +0700

    Update entries factory

--- spec/factories/entries.rb
+++ spec/factories/entries.rb
@@ -1,7 +1,18 @@
 FactoryBot.define do
-  factory :entry do
-    account { nil }
-    entryable { nil }
-    status { "MyString" }
+  factory :entry, aliases: [:article_entry, :comment_entry, :message_entry] do
+    account
+    status { 'active' }
+
+    trait :article do
+      entryable factory: :article
+    end
+
+    trait :comment do
+      entryable factory: :comment
+    end
+
+    trait :message do
+      entryable factory: :message
+    end
   end
 end
