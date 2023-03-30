706d9c4295b21bdb3962701a92bdca2d3526598f
Trang Tung Nguyen <trangtungn@gmail.com>
2023-03-31 01:55:11 +0700

    Update seeds to create message entry with nested attribute

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,11 @@ comment_entry = FactoryBot.create(:comment_entry, account:, entryable: comment)
 message = FactoryBot.create(:message)
 message_entry = FactoryBot.create(:message_entry, account:, entryable: message)
 
+# create entry using Nested Attributes
+params = { entry: { entryable_type: 'Message', entryable_attributes: { subject: 'Smiling' } } }
+message_entry_with_nested_subject = Entry.create(params[:entry])
+message_entry_with_nested_subject.entryable.id # => 2
+message_entry_with_nested_subject.entryable.subject # => 'Smiling'
+
 task = FactoryBot.create(:task)
 task_of_account = FactoryBot.create(:task, account:)
