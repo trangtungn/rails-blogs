f4b76f0fcb219cfa5f6d33879d5c49d21cb4bebd
Trang Tung Nguyen <trangtungn@gmail.com>
2023-03-31 13:51:47 +0700

    Remove unused tests

--- test/controllers/articles_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class ArticlesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get articles_path
-    assert_response :success
-  end
-end
