e163b45d342e8659016a6a1722f0a906930653eb
Trang Tung Nguyen <trangtungn@gmail.com>
2023-03-31 13:54:52 +0700

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,23 @@
-account = FactoryBot.create(:account)
+account = FactoryBot.create(:account, :signin)
 
 article = FactoryBot.create(:article)
-article_entry = FactoryBot.create(:article_entry, account:, entryable: article)
-article_entry2 = FactoryBot.create(:article_entry, :article)
+FactoryBot.create(:article_entry, account:, entryable: article)
 
-comment = FactoryBot.create(:comment)
-comment_entry = FactoryBot.create(:comment_entry, account:, entryable: comment)
+comment = FactoryBot.create(:comment, article:)
+FactoryBot.create(:comment_entry, account:, entryable: comment)
 
 message = FactoryBot.create(:message)
-message_entry = FactoryBot.create(:message_entry, account:, entryable: message)
+FactoryBot.create(:message_entry, account:, entryable: message)
 
 # create entry using Nested Attributes
-params = { entry: { entryable_type: 'Message', entryable_attributes: { subject: 'Smiling' } } }
-message_entry_with_nested_subject = Entry.create(params[:entry])
-message_entry_with_nested_subject.entryable.id # => 2
-message_entry_with_nested_subject.entryable.subject # => 'Smiling'
+params = {
+  entry: {
+    entryable_type: 'Message',
+    entryable_attributes: { subject: 'Smiling' }, account:,
+  },
+}
+message_entry_with_nested_subject = Entry.create!(params[:entry])
+p message_entry_with_nested_subject.entryable.id # => 2
+p message_entry_with_nested_subject.entryable.subject # => 'Smiling'
 
-task = FactoryBot.create(:task)
-task_of_account = FactoryBot.create(:task, account:)
+FactoryBot.create(:task, account:)
